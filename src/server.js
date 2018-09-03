d3c12f69a6be20fb5cda804450fc7161c3a0a16f
stefano <stefanorossi.tigmail.com>
2018-09-03 13:49:20 -0700

    example serving static assets with webpack and nodeJS

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const Express = require('express')
-const app = new Express()
+const path = require('path')
+const fs = require('fs')
 
+const app = new Express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('random answer')
+    const pathToHtml = path.resolve(__dirname, '../dist/hello-world.html')
+    const contentOfHtmlFile = fs.readFileSync(pathToHtml, 'utf-8')
+
+    res.send(contentOfHtmlFile)
 })
+
+app.use('/static', Express.static(path.resolve(__dirname, '../dist')))
 
 app.listen(port, () => {
     console.log('Application is running on httl://localhost:' + port)
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].[contenthash].js',
-        publicPath: './'
+        publicPath: '/static/'
     },
     mode: 'production',
     optimization: {
